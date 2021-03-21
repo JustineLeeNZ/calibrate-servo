8ac4fc3b0a2e9be9ab1c4de53408ccaefa701f4f
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-03-21 19:06:02 +1300

    added Button B and A+B for extra angle values

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(60)
+})
+input.onButtonPressed(Button.AB, function () {
     servos.P0.setAngle(90)
+})
+input.onButtonPressed(Button.B, function () {
+    servos.P0.setAngle(120)
 })
 basic.showIcon(IconNames.StickFigure)
