eaa69f034ca7a2e1f373a48ee5d593f3afb83b9e
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2021-03-21 22:15:07 +1300

    Changed angles

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    servos.P0.setAngle(60)
+    servos.P0.setAngle(95)
 })
 input.onButtonPressed(Button.AB, function () {
     servos.P0.setAngle(90)
 })
 input.onButtonPressed(Button.B, function () {
-    servos.P0.setAngle(120)
+    servos.P0.setAngle(175)
 })
 basic.showIcon(IconNames.StickFigure)
